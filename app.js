7a85684dabfda7686a357bb13005a67a40b20bb9
IvanLezcano <lezcanoivangabriel@gmail.com>
2021-07-09 12:02:01 -0300

    commit del tp

---
+++ app.js
@@ -0,0 +1,25 @@
+const { static } = require("express");
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+const views = path.join(__dirname,"views")
+console.log(__dirname);
+app.use(express.static('public'));
+
+
+app.get("/",(req,res) => res.sendFile(path.join(views,"home.html")))
+app.get("/login",(req,res) => res.sendFile(path.join(views,"login.html")))
+app.get("/registrarse",(req,res) => res.sendFile(path.join(views,"registro.html")))
+app.get("/registroexitoso",(req,res) => res.sendFile(path.join(views,"hola.html")))
+app.get("/logeoexitoso",(req,res) => res.sendFile(path.join(views,"hola2.html")))
+
+
+
+
+
+
+
+app.listen(port,() => console.log("El servidor corriendo en el puerto"+ port))
+
+//npx nodemon app.js para ejecutarlo
